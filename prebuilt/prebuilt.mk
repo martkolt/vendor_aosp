2dc4e1dea28f02d7c11e6f528e231f8ddd4e9b20
Josh Chasky <jchasky@gmail.com>
2016-09-08 19:39:41 -0400

    Allow AOSP KB to gesture type
    - Add condition for 32/64 devices
    
    Change-Id: Ia19043df237f41ebe0ff556fe794eb115d6ec212

--- prebuilt/prebuilt.mk
+++ prebuilt/prebuilt.mk
@@ -9,3 +9,14 @@ PRODUCT_COPY_FILES += \
 # World APN list
 PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# LatinIME gesture typing
+ifeq ($(TARGET_ARCH),arm64)
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so \
+    vendor/aosp/prebuilt/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so
+else
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/lib/libjni_latinime.so:system/lib/libjni_latinime.so \
+    vendor/aosp/prebuilt/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
+endif
