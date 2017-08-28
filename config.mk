f13da2da41e4aab2762120912d387170f3b30e0e
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-08-28 15:19:18 -0300

    Initial vendor Based on PureNexusProject

---
+++ config.mk
@@ -0,0 +1,44 @@
+# Overlays
+PRODUCT_PACKAGE_OVERLAYS += vendor/aosp/overlay/common
+
+# Include explicitly to work around GMS issues
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full
+
+# Telephony packages
+PRODUCT_PACKAGES += \
+    Stk
+
+# Allow tethering without provisioning app
+PRODUCT_PROPERTY_OVERRIDES += \
+    net.tethering.noprovisioning=true
+
+# Thank you, please drive thru!
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.dun.override=0
+
+# Google property overides
+PRODUCT_PROPERTY_OVERRIDES += \
+    keyguard.no_require_sim=true \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.com.google.clientidbase=android-google \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.error.receiver.system.apps=com.google.android.gms \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.com.android.dataroaming=false \
+    ro.atrace.core.services=com.google.android.gms,com.google.android.gms.ui,com.google.android.gms.persistent \
+    ro.setupwizard.rotation_locked=true
+
+# Security Enhanced Linux
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.selinux=1
+
+# Don't compile SystemUITests
+EXCLUDE_SYSTEMUI_TESTS := true
+
+# Version
+CUSTOM_VERSION := $(PLATFORM_VERSION)-$(shell date +%Y%m%d)-mod
+
+$(call inherit-product-if-exists, vendor/aosp/prebuilt/prebuilt.mk)
+$(call inherit-product-if-exists, vendor/aosp/sepolicy/sepolicy.mk)
