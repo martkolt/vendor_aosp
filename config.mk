250ab1a4f35aa5e597bd2742a9c2ed5ca79b7b48
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-08-28 16:22:55 -0300

    Bring APNs from LOS

--- config.mk
+++ config.mk
@@ -40,6 +40,9 @@ EXCLUDE_SYSTEMUI_TESTS := true
 # Bootanimation
 PRODUCT_COPY_FILES += vendor/aosp/prebuilt/media/bootanimation.zip:system/media/bootanimation.zip
 
+# World APN list
+PRODUCT_COPY_FILES += vendor/aosp/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
+
 # Version
 CUSTOM_VERSION := $(PLATFORM_VERSION)-$(shell date +%Y%m%d)-mod
 
