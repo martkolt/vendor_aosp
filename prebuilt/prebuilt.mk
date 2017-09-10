9998f8fb59311081b45b51b59eabe3750119bdd4
Josh Chasky <jchasky@gmail.com>
2017-09-10 12:41:51 -0400

    Fix AOSP gesture typing
    Change-Id: Ia19043df237f41ebe0ff556fe794eb115d6ec303

--- prebuilt/prebuilt.mk
+++ prebuilt/prebuilt.mk
@@ -11,12 +11,12 @@ PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
 
 # LatinIME gesture typing
-ifeq ($(TARGET_ARCH),arm64)
-PRODUCT_COPY_FILES += \
-    vendor/aosp/prebuilt/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so \
-    vendor/aosp/prebuilt/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so
-else
+ifneq ($(filter shamu,$(TARGET_PRODUCT)),)
 PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/lib/libjni_latinime.so:system/lib/libjni_latinime.so \
     vendor/aosp/prebuilt/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
+else
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so \
+    vendor/aosp/prebuilt/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so
 endif
