4117d8dc600b764f6e6c8fe1f13c58c4541ef37e
YumeMichi <do4suki@gmail.com>
2017-09-16 23:06:30 +0000

    Add OnePlus X OOS3 Camera
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- prebuilt/prebuilt.mk
+++ prebuilt/prebuilt.mk
@@ -2,10 +2,24 @@
 PRODUCT_PACKAGES += \
     Turbo \
     Launcher3 \
-    Phonograph
+    Phonograph \
+    OnePlusCamera \
+    OnePlusCameraService
 
 # Bootanimation
-PRODUCT_COPY_FILES += vendor/aosp/prebuilt/media/bootanimation.zip:system/media/bootanimation.zip
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/media/bootanimation.zip:system/media/bootanimation.zip
 
 # World APN list
-PRODUCT_COPY_FILES += vendor/aosp/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# OnePlus Camera
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/framework/org.codeaurora.camera.jar:system/framework/org.codeaurora.camera.jar \
+    vendor/aosp/prebuilt/bin/secure_camera_sample_client:system/bin/secure_camera_sample_client \
+    vendor/aosp/prebuilt/lib/libfilter-sdk.so:system/lib/libfilter-sdk.so \
+    vendor/aosp/prebuilt/lib/libopbaselib.so:system/lib/libopbaselib.so \
+    vendor/aosp/prebuilt/lib/libopcamera.so:system/lib/libopcamera.so \
+    vendor/aosp/prebuilt/lib/libopcameralib.so:system/lib/libopcameralib.so \
+    vendor/aosp/prebuilt/priv-app/OnePlusCamera/lib/arm/libopbaselib.so:system/priv-app/OnePlusCamera/lib/arm/libopbaselib.so
