21cd7e66fac757db6b6b0bd6b932a817b68d7725
YumeMichi <do4suki@gmail.com>
2017-09-16 22:39:04 +0800

    vendor: Cleanup prebuilt apks
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- config.mk
+++ config.mk
@@ -42,12 +42,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Don't compile SystemUITests
 EXCLUDE_SYSTEMUI_TESTS := true
 
-# Bootanimation
-PRODUCT_COPY_FILES += vendor/aosp/prebuilt/media/bootanimation.zip:system/media/bootanimation.zip
-
-# World APN list
-PRODUCT_COPY_FILES += vendor/aosp/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
-
 # Set Pixel blue light theme on Gboard
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.ime.theme_id=5
--- prebuilt/prebuilt.mk
+++ prebuilt/prebuilt.mk
@@ -3,3 +3,9 @@ PRODUCT_PACKAGES += \
     Turbo \
     Launcher3 \
     Phonograph
+
+# Bootanimation
+PRODUCT_COPY_FILES += vendor/aosp/prebuilt/media/bootanimation.zip:system/media/bootanimation.zip
+
+# World APN list
+PRODUCT_COPY_FILES += vendor/aosp/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
