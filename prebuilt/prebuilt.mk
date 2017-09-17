df9337a5a6c2749926253027fe422b4e7be893cd
YumeMichi <do4suki@gmail.com>
2017-09-17 17:35:25 +0000

    Kill OSS3 Camera
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- prebuilt/prebuilt.mk
+++ prebuilt/prebuilt.mk
@@ -3,8 +3,6 @@ PRODUCT_PACKAGES += \
     Turbo \
     Launcher3 \
     Phonograph \
-    OnePlusCamera \
-    OnePlusCameraService \
     FirefoxFocus
 
 # Bootanimation
@@ -14,13 +12,3 @@ PRODUCT_COPY_FILES += \
 # World APN list
 PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
-
-# OnePlus Camera
-PRODUCT_COPY_FILES += \
-    vendor/aosp/prebuilt/framework/org.codeaurora.camera.jar:system/framework/org.codeaurora.camera.jar \
-    vendor/aosp/prebuilt/bin/secure_camera_sample_client:system/bin/secure_camera_sample_client \
-    vendor/aosp/prebuilt/lib/libfilter-sdk.so:system/lib/libfilter-sdk.so \
-    vendor/aosp/prebuilt/lib/libopbaselib.so:system/lib/libopbaselib.so \
-    vendor/aosp/prebuilt/lib/libopcamera.so:system/lib/libopcamera.so \
-    vendor/aosp/prebuilt/lib/libopcameralib.so:system/lib/libopcameralib.so \
-    vendor/aosp/prebuilt/priv-app/OnePlusCamera/lib/arm/libopbaselib.so:system/priv-app/OnePlusCamera/lib/arm/libopbaselib.so
