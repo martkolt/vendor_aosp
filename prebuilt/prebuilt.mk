356f2ae8d72b37f0df55bb53c9670b9d91ca0aee
YumeMichi <do4suki@gmail.com>
2018-01-28 22:59:50 +0000

    [3/3] vendor: Cleanup unused charging alert feature
    Revert "[3/3] vendor: Introduce charging alerts"
    
    This reverts commit f92ff3a8004643bb199bab7d0fa41ba40eb79e57.
    
    Revert "Import charging sounds"
    
    This reverts commit e072c2dbc0d241fe238d549cb71d3b2c3317becf.
    
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- prebuilt/prebuilt.mk
+++ prebuilt/prebuilt.mk
@@ -20,9 +20,3 @@ PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so \
     vendor/aosp/prebuilt/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so
 endif
-
-# Charging sounds
-PRODUCT_COPY_FILES += \
-    vendor/aosp/prebuilt/media/google/effects/BatteryPlugged.ogg:system/media/audio/ui/BatteryPlugged.ogg \
-    vendor/aosp/prebuilt/media/google/effects/BatteryPlugged_48k.ogg:system/media/audio/ui/BatteryPlugged_48k.ogg \
-    vendor/aosp/prebuilt/media/google/effects/GoodBattery.ogg:system/media/audio/ui/GoodBattery.ogg
