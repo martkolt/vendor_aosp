9b5ad9ff33a4e3846c2965f3ffbf9da67bf08b49
YumeMichi <do4suki@gmail.com>
2018-04-12 10:12:30 +0800

    vendor: Add vendor envsetup.sh
    * Squashed commits:
    
    build: Add mka/brunch/breakfast aliases
    
    - bacon target is move to its own Makefile
    
    Change-Id: I06d70a56c6946d7a23c6df81769aa20e7500f953
    
    envsetup: Add pushboot function
    
    This function lets you push a file from your current OUT dir
    for your current device. It root/remount the device, push the
    file, and reboots it. Example: pushboot system/app/SystemUI.apk
    
    Change-Id: I8a404323a4091e8502fbdd24198c66f1ac598192
    
    Build: Add support for specifying build variant in brunch/breakfast
    
    This little modification allows specifying build variant in brunch/breakfast commands. For example we can use "brunch i9300 user" to build user variant instead of default userdebug.
    
    When no extra argument is given, userdebug is default.
    
    Change-Id: I2d438209f8a14949c64baa66485c811da384d20b
    
    envsetup: add a helper to fix "out/target/common" not being really common
    
    * out/target/common isn't really common, and if you do
      back to back builds for multiple devices, that is noticable.
    * Use out/target/common-$device instead, and link the appropriate
      dir to out/target/common every time lunch() is run, if
      ANDROID_FIXUP_COMMON_OUT is set.
    * Refer https://groups.google.com/forum/#!topic/android-building/ispbOgzoyg8
      for more info.
    
    Change-Id: I11e7df0e68e2a60ce32576f06397d60fc9465b60
    
    envsetup: Update mka for O
    
    Change-Id: I36ec10be9152f135ec3645b4304e63ea30d06774
    
    vendor: build: Kill schedtool
    
    Change-Id: Idd6cc6679b1e84f365a5a18645590c549af20af5
    
    vendor: build. -j is no longer supported
    
    Change-Id: I3840b696066d1211e77ef3600fe069ff3916cc6e
    
    vendor: add aospremote and cafremote functions
    
    Change-Id: I21318786dde838a51a2672c1af7cfd32ee8c1c8b

---
+++ build/envsetup.sh
@@ -0,0 +1,145 @@
+function __print_aosp_functions_help() {
+cat <<EOF
+Additional AOSP functions:
+- breakfast:       Setup the build environment, but only list
+                   devices we support.
+- brunch:          Sets up build environment using breakfast(),
+                   and then comiles using mka() against bacon target.
+- mka:             Builds using SCHED_BATCH on all processors.
+- pushboot:        Push a file from your OUT dir to your phone and
+                   reboots it, using absolute path.
+EOF
+}
+
+function brunch()
+{
+    breakfast $*
+    if [ $? -eq 0 ]; then
+        time mka bacon
+    else
+        echo "No such item in brunch menu. Try 'breakfast'"
+        return 1
+    fi
+    return $?
+}
+
+function breakfast()
+{
+    target=$1
+    local variant=$2
+    CUSTOM_DEVICES_ONLY="true"
+    unset LUNCH_MENU_CHOICES
+    add_lunch_combo full-eng
+    for f in `/bin/ls device/*/*/vendorsetup.sh 2> /dev/null`
+        do
+            echo "including $f"
+            . $f
+        done
+    unset f
+
+    if [ $# -eq 0 ]; then
+        # No arguments, so let's have the full menu
+        lunch
+    else
+        echo "z$target" | grep -q "-"
+        if [ $? -eq 0 ]; then
+            # A buildtype was specified, assume a full device name
+            lunch $target
+        else
+            # This is probably just the aosp model name
+            if [ -z "$variant" ]; then
+                variant="userdebug"
+            fi
+            lunch aosp_$target-$variant
+        fi
+    fi
+    return $?
+}
+
+alias bib=breakfast
+
+function fixup_common_out_dir() {
+    common_out_dir=$(get_build_var OUT_DIR)/target/common
+    target_device=$(get_build_var TARGET_DEVICE)
+    if [ ! -z $ANDROID_FIXUP_COMMON_OUT ]; then
+        if [ -d ${common_out_dir} ] && [ ! -L ${common_out_dir} ]; then
+            mv ${common_out_dir} ${common_out_dir}-${target_device}
+            ln -s ${common_out_dir}-${target_device} ${common_out_dir}
+        else
+            [ -L ${common_out_dir} ] && rm ${common_out_dir}
+            mkdir -p ${common_out_dir}-${target_device}
+            ln -s ${common_out_dir}-${target_device} ${common_out_dir}
+        fi
+    else
+        [ -L ${common_out_dir} ] && rm ${common_out_dir}
+        mkdir -p ${common_out_dir}
+    fi
+}
+
+# Make using all available CPUs
+function mka() {
+    m "$@"
+}
+
+function pushboot() {
+    if [ ! -f $OUT/$* ]; then
+        echo "File not found: $OUT/$*"
+        return 1
+    fi
+
+    adb root
+    sleep 1
+    adb wait-for-device
+    adb remount
+
+    adb push $OUT/$* /$*
+    adb reboot
+}
+
+function aospremote()
+{
+    if ! git rev-parse --git-dir &> /dev/null
+    then
+        echo ".git directory not found. Please run this from the root directory of the Android repository you wish to set up."
+        return 1
+    fi
+    git remote rm aosp 2> /dev/null
+    local PROJECT=$(pwd -P | sed -e "s#$ANDROID_BUILD_TOP\/##; s#-caf.*##; s#\/default##")
+    # Google moved the repo location in Oreo
+    if [ $PROJECT = "build/make" ]
+    then
+        PROJECT="build"
+    fi
+    if (echo $PROJECT | grep -qv "^device")
+    then
+        local PFX="platform/"
+    fi
+    git remote add aosp https://android.googlesource.com/$PFX$PROJECT
+    echo "Remote 'aosp' created"
+}
+
+function cafremote()
+{
+    if ! git rev-parse --git-dir &> /dev/null
+    then
+        echo ".git directory not found. Please run this from the root directory of the Android repository you wish to set up."
+        return 1
+    fi
+    git remote rm caf 2> /dev/null
+    local PROJECT=$(pwd -P | sed -e "s#$ANDROID_BUILD_TOP\/##; s#-caf.*##; s#\/default##")
+     # Google moved the repo location in Oreo
+    if [ $PROJECT = "build/make" ]
+    then
+        PROJECT="build"
+    fi
+    if [[ $PROJECT =~ "qcom/opensource" ]];
+    then
+        PROJECT=$(echo $PROJECT | sed -e "s#qcom\/opensource#qcom-opensource#")
+    fi
+    if (echo $PROJECT | grep -qv "^device")
+    then
+        local PFX="platform/"
+    fi
+    git remote add caf https://source.codeaurora.org/quic/la/$PFX$PROJECT
+    echo "Remote 'caf' created"
+}
